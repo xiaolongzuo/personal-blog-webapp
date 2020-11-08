5bfddfb8484d319238aa5298c34f71725f1c8a39
zuoxiaolong <150349407@qq.com>
2020-11-08 18:37:01 +0800

    polish

--- auto_backup_db.sh
+++ auto_backup_db.sh
@@ -7,17 +7,17 @@ fi
 
 PASSWORD=$1
 
-cp /home/xiaolongzuo/db_backup_blog.sql /home/xiaolongzuo/db_backup_blog.sql.bak
-cp /home/xiaolongzuo/app_backup_blog.tar.gz /home/xiaolongzuo/app_backup_blog.tar.gz.bak
+cp /home/zuoxiaolong/backup/db_backup_blog.sql /home/zuoxiaolong/backup/db_backup_blog.sql.bak
+cp /home/zuoxiaolong/backup/app_backup_blog.tar.gz /home/zuoxiaolong/backup/app_backup_blog.tar.gz.bak
 
-rm -f /home/xiaolongzuo/db_backup_blog.sql
-rm -f /home/xiaolongzuo/app_backup_blog.tar.gz
+rm -f /home/zuoxiaolong/backup/db_backup_blog.sql
+rm -f /home/zuoxiaolong/backup/app_backup_blog.tar.gz
 
-mysqldump -uroot -p$PASSWORD blog > /home/xiaolongzuo/db_backup_blog.sql
-tar -zcvf /home/xiaolongzuo/app_backup_blog.tar.gz /home/xiaolongzuo/zuoxiaolong/
+mysqldump -uroot -p$PASSWORD blog > /home/zuoxiaolong/backup/db_backup_blog.sql
+tar -zcvf /home/zuoxiaolong/backup/app_backup_blog.tar.gz /home/zuoxiaolong/personal-blog-webapp-site/
 
-chown xiaolongzuo.xiaolongzuo /home/xiaolongzuo/db_backup_blog.sql
-chown xiaolongzuo.xiaolongzuo /home/xiaolongzuo/app_backup_blog.tar.gz
+chown zuoxiaolong.zuoxiaolong /home/zuoxiaolong/backup/db_backup_blog.sql
+chown zuoxiaolong.zuoxiaolong /home/zuoxiaolong/backup/app_backup_blog.tar.gz
 
-rm -f /home/xiaolongzuo/db_backup_blog.sql.bak
-rm -f /home/xiaolongzuo/app_backup_blog.tar.gz.bak
+rm -f /home/zuoxiaolong/backup/db_backup_blog.sql.bak
+rm -f /home/zuoxiaolong/backup/app_backup_blog.tar.gz.bak
